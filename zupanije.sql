83596a91dfc47313614f08f1d17ab58f68a08dc1
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-12 15:40:35 +0200

    Zupanije (sa predavanja)

---
+++ zupanije.sql
@@ -0,0 +1,37 @@
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca11_5\zupanije.sql
+
+drop database if exists zupanije;
+create database zupanije;
+use zupanije;
+
+create table zupanija(
+    sifra int not null primary key auto_increment, 
+    naziv varchar(50) not null,
+    zupan varchar(50)
+);
+
+create table opcina(
+    sifra int not null primary key auto_increment, 
+    zupanija int not null,
+    naziv varchar(50) not null
+);
+
+alter table opcina add foreign key (zupanija) references zupanija (sifra);
+insert into zupanija (naziv)
+values ('Osječko -baranjska' );
+
+insert into zupanija(naziv)
+values ('Dubrovačko-Neretvanska');
+insert into zupanija(naziv)
+values ('Grad Zagreb');
+
+insert into opcina (naziv, zupanija)
+values ('Osijek',1);
+
+insert into opcina (naziv, zupanija)
+values ('Valpovo',1);
+
+
+
+
+
