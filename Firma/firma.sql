4148681089784e5d2203b62dcd6172f9bc39cf75
Ivan Stanić <104570211+IvanStanic84@users.noreply.github.com>
2022-05-13 11:33:20 +0200

    Promjena adrese

--- Firma/firma.sql
+++ Firma/firma.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca11_5\firma.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca11_5\Firma\firma.sql
 
 drop database if exists firma;
 create database firma;
--- Hijerarhija/hijerarhija.sql
+++ Hijerarhija/hijerarhija.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca11_5\hijerarhija.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\zadaca11_5\Hijerarhija\hijerarhija.sql
 
 drop database if exists hijerarhija;
 create database hijerarhija;
